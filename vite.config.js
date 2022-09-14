68470cef8e70f2e60ff3f7df4ce23c45377adb4b
JavGt <javier.bz.gt@gmail.com>
2022-09-14 17:51:16 -0500

    add port

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -8,5 +8,11 @@ export default defineConfig({
 		open: true,
 		host: true,
 		port: 3000,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@components': path.resolve(__dirname, 'src/components'),
+		},
 	},
 });
